a51259f39e7991ee41ed7d8ac0b43f05531a79ad
Gaurav verma <94301843+abcdeCoder@users.noreply.github.com>
2022-12-05 22:00:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -26,14 +26,14 @@ app.use('/api/v1', chat);
 app.use('/api/v1', message);
 
 // deployment
-// __dirname = path.resolve();
+__dirname = path.resolve();
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('frontend/build'));
         console.log('production');
-    // app.get('*', (req, res) => {
-    //     res.sendFile(path.resolve( 'frontend', 'build', 'index.html'))
-    //     console.log('production2');
-    // });
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+        console.log('production2');
+    });
 } else {
     app.get('/', (req, res) => {
         res.send('Server is Running! 🚀');
